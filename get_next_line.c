c33bebf30050b7e26b26837316599d2394c0d8dd
Cypher Nex <cypher-nex@localhost.localdomain>
2023-08-22 00:07:07 +0100

    done

--- get_next_line.c
+++ get_next_line.c
@@ -11,24 +11,25 @@ char    *get_next_line(int fd)
   if (fd < 0 || BUFFER_SIZE <= 0)
     return NULL;
 
-  while ((newline = ft_strchr(remainder, '\n')) == NULL && (bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0)
+  while ((remainder == NULL || (newline = ft_strchr(remainder, '\n')) == NULL) && (bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0)
   {
     buffer[bytes_read] = '\0';
-    remainder = ft_strjoin(remainder, buffer);
+    if (remainder == NULL)
+      remainder = ft_strdup(buffer);
+    else
+      remainder = ft_strjoin(remainder, buffer);
   }
 
   if (newline)
   {
     *newline = '\0';
-    line = ft_strdup(remainder);
-    newline = ft_strdup(newline + 1);
-    free(remainder);
-    remainder = newline;
+    line = ft_strnew(newline - remainder);
+    ft_strncpy(line, remainder, newline - remainder);
+    remainder = ft_strdup(newline + 1);
   }
   else if (!bytes_read && remainder)
   {
     line = ft_strdup(remainder);
-    free(remainder);
     remainder = NULL;
   }
 
---
+++ get_next_line_bonus.c
@@ -0,0 +1,37 @@
+#include "get_next_line_bonus.h"
+
+char    *get_next_line(int fd)
+{
+  static char   *remainder[1024] = {NULL};
+  char          buffer[BUFFER_SIZE + 1];
+  int           bytes_read = 0;
+  char          *newline = NULL;
+  char          *line = NULL;
+
+  if (fd < 0 || BUFFER_SIZE <= 0)
+    return NULL;
+
+	while ((!remainder[fd] || (newline = ft_strchr(remainder[fd], '\n')) == NULL) && (bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0)
+  {
+    buffer[bytes_read] = '\0';
+		if (remainder[fd] == NULL)
+			remainder[fd] = ft_strdup(buffer);
+		else
+		  remainder[fd] = ft_strjoin(remainder[fd], buffer);
+  }
+
+  if (newline)
+{
+		*newline = '\0';
+    line = ft_strnew(newline - remainder[fd]);
+    ft_strncpy(line, remainder[fd], newline - remainder[fd]);
+    remainder[fd] = ft_strdup(newline + 1);
+}
+  else if (!bytes_read && remainder[fd])
+  {
+    line = ft_strdup(remainder[fd]);
+    remainder[fd] = NULL;
+  }
+
+  return line;
+}
